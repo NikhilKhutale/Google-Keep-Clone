72037cf799d2396103f68dc26302be303e00bd84
Nikhil Khutale <nikhilkhutale500@gmail.com>
2022-11-22 11:02:23 +0530

    first commit

--- db.js
+++ db.js
@@ -8,5 +8,6 @@ export const db = mysql.createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATA
+    database:process.env.DB_DATA,
+    ssl:process.env.CERT
 })
